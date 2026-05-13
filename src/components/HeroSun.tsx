5729adff7ffbc59e78eccb65c00ed88f5ef8e16a
Paul <paul@suedeapple.co.uk>
2026-05-13 17:33:18 +0100

    Remove unused scripts and assets related to fun facts, puns, and Claude API interactions; update layout copyright year dynamically; delete HeroSun and Nav components along with their styles.

--- src/components/HeroSun.tsx
+++
@@ -1,6 +0,0 @@
-import SunSvg from "../../public/sun.svg";
-import styles from "./HeroSun.module.css";
-
-export default function HeroSun() {
-  return <SunSvg className={styles.sun} aria-hidden="true" />;
-}
